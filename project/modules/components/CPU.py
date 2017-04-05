19f2ecf6c30c24bbeca5f1e3bc2e99f748d3573c
Popolo <devnull@localhost>
2017-04-05 11:29:19 -0400

    CPU functions done

--- project/modules/components/CPU.py
+++ project/modules/components/CPU.py
@@ -1,13 +1,20 @@
 # -*- coding:utf8 -*-
+from ..Utilities import int16
 from .IComponent import *
-from .ALU import *
+from .ALU import
 
 class CPU(IComponent):
     """CENTRAL PROCESSING UNIT"""
 
+    STATUS_PARITY = 0
+    STATUS_SIGN = 1
+    STATUS_CARRY = 2
+    STATUS_ZERO = 3
+    STATUS_CND = 4
+
     def __init__(self, bus):
         super().__init__(bus)
-        self.ALU = ALU()
+        self.ALU = ALU() # Inutile dans notre implémentation ?
 
         # A B C D
         self.REGISTERS = [0 for i in range(4)]
@@ -17,6 +24,13 @@ class CPU(IComponent):
         self.PC = 0
         self.IR = 0
 
+    def getStatusBit(self, bit):
+        return int16.BitGet(self.STATUS, bit)
+
+    def setStatusBit(self, bit, val):
+        self.STATUS = int16.BitSet(self.STATUS, bit, val)
+        return self
+
     def getRegister(self, reg):
         """Register is from 0x01 to 0x04,
         but indices range from 0 to 3..."""
@@ -25,8 +39,17 @@ class CPU(IComponent):
             raise ValueError('Register at 0x%x is invalid' % reg)
         return self.REGISTERS[i]
 
+    def setRegister(self, reg, val):
+        i = reg - 1
+        self.REGISTERS[i] = val
+        return self
+
     def getShared(self, adr):
         """Get data through the BUS"""
+        pass
+
+    def setShared(self, adr, val):
+        """Set data through the BUS"""
         pass
 
     def fetch(self):
@@ -51,7 +74,7 @@ class CPU(IComponent):
         except AttributeError: # Empty function
             return lambda *args, **kargs: None
 
-    def Managed(*types, store=None):
+    def Managed(*types, store=None, status=None):
         """This ensure each function affects correctly the different CPU registers.
         Also allows for fuzzy arguments conversion.
         Types:
@@ -95,21 +118,92 @@ class CPU(IComponent):
                 # Call with converted args the function
                 value = func(*cargs)
 
-                # Check for overflow
-                if value > 65536:
-                    value %= 65536
+                # Check for parity
+                parity = int16.BitGet(0)
+                self.setStatusBit(self.STATUS_PARITY, parity)
 
-                if stored is not None:
+                # Check sign
+                sign = 0
+                if value < 0:
+                    value = abs(value)
+                    sign = 1
+                self.setStatusBit(self.STATUS_CARRY, sign)
+
+                # Check for overflow
+                overflow, cleanValue = int16.Overflow(value)
+                self.setStatusBit(self.STATUS_CARRY, int(overflow))
+
+                if status is not None:
+                    self.setStatusBit(status, cleanValue)
+
+                if store is not None:
                     i = stored - 1
                     if types[i] not in TYPES['register']:
                         raise TypeError("Can't store your shit")
-                    self.setRegister(args[i], value)
+                    self.setRegister(args[i], cleanValue)
 
                 return value
 
             return modified
 
         return decorator
+
+    ###
+    # Data manipulation
+
+    @Managed('r', 'v', store=1)
+    def SET(self, reg, val): return val
+
+    @Managed('r', 'a', store=1)
+    def LD(self, reg, adr): return adr
+
+    @Managed('r', 'v') # Little hack for setting distant address
+    def ST(self, reg, adr): self.setShared(adr, reg)
+
+    @Managed('r', 'r', store=1)
+    def MV(self, reg1, reg2): return reg2
+
+    ###
+    # Comparisons
+
+    @Managed('r', 'r', status=STATUS_CND)
+    def LT(self, reg1, reg2): return int(reg1 < reg2)
+
+    @Managed('r', 'r', status=STATUS_CND)
+    def GT(self, reg1, reg2): return int(reg1 > reg2)
+
+    @Managed('r', 'r', status=STATUS_CND)
+    def LE(self, reg1, reg2): return int(reg1 <= reg2)
+
+    @Managed('r', 'r', status=STATUS_CND)
+    def GE(self, reg1, reg2): return int(reg1 >= reg2)
+
+    @Managed('r', 'r', status=STATUS_CND)
+    def EQ(self, reg1, reg2): return int(reg1 == reg2)
+
+    @Managed('r', status=STATUS_CND)
+    def EZ(self, reg1): return int(reg1 == 0)
+
+    @Managed('r', status=STATUS_CND)
+    def NZ(self, reg1): return int(reg1 != 0)
+
+    ###
+    # Logic
+
+    @Managed('r', 'r', store=1)
+    def OR(self, reg1, reg2): return reg1 | reg2
+
+    @Managed('r', 'r', store=1)
+    def AND(self, reg1, reg2): return reg1 & reg2
+
+    @Managed('r', 'r', store=1)
+    def XOR(self, reg1, reg2): return reg1 ^ reg2
+
+    @Managed('r', store=1)
+    def NOT(self, reg1): return int16.Inverse(reg1)
+
+    ###
+    # Basic operations
 
     @Managed('r', 'r', store=1)
     def ADD(self, reg1, reg2): return reg1 + reg2
@@ -123,7 +217,32 @@ class CPU(IComponent):
     @Managed('r', 'r', store=1)
     def DIV(self, reg1, reg2): return reg1 / reg2
 
+    ###
+    # Flow control
+
     @Managed('a')
     def JMP(self, adr):
         self.IR = adr
         return adr
+
+    @Managed('a')
+    def JMZ(self, adr):
+        if self.getStatusBit(self.STATUS_ZERO):
+            self.IR = adr
+        return adr
+
+    @Managed('a')
+    def JMO(self, adr):
+        if self.getStatusBit(self.STATUS_CARRY):
+            self.IR = adr
+        return adr
+
+    @Managed('a')
+    def JMC(self, adr):
+        if self.getStatusBit(self.STATUS_CND):
+            self.IR = adr
+        return adr
+
+    # \o/
+    def NOP(self): pass
+    def HALT(self): pass
---
+++ project/modules/utilities/int16.py
@@ -0,0 +1,20 @@
+# -*- coding:utf8 -*-
+
+MAX = 1<<16
+MIN = 0
+
+def Overflow(n):
+    return (n > MAX, n % MAX)
+
+def BitGet(n, bit):
+    mask = 1 << bit
+    return n & mask
+
+def BitSet(n, bit, val):
+    mask = 1 << bit
+    m = n & (MAX - 1 ^ mask)
+    m = m | val << bit
+    return m
+
+def Inverse(n):
+    return MAX-1 ^ n

b12b19b4b08eaf2c88de098d9b19405f3f73425f
Popolo <devnull@localhost>
2017-04-18 18:19:48 -0400

    VM exe fix

--- VM.py
+++ VM.py
@@ -3,7 +3,7 @@ import project.modules.ArgParser as ArgParser
 
 from project.modules.VirtualMachine import *
 
-filename = ArgParser.getArg('file', ArgParser.getPos(0))
+filename = ArgParser.getArg('file', ArgParser.getPos(1))
 if filename is None: raise Exception('First argument must be the compiled file to run.')
 
 VirtualMachine.SpawnAndExecute(filename)
